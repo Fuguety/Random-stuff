cf3b2255c93ae979273cb6535c8d9fe3f2d56402
Lucas Azevedo <102688981+Fuguety@users.noreply.github.com>
2023-01-07 13:20:46 -0300

    Update SDL_MovingRectangle + CollisionOnWalls.cpp

--- SDL/SDL_MovingRectangle + CollisionOnWalls.cpp
+++ SDL/SDL_MovingRectangle + CollisionOnWalls.cpp
@@ -74,10 +74,6 @@ int main(int argc, char* argv[])
 
 	SDL_Renderer* render = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-    // Color of window
-    //SDL_SetRenderDrawColor(render, 255, 255, 255, 255);
-    SDL_RenderClear(render);
-
 
 	bool running = true;
 	SDL_Event ev;
