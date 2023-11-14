59e532eb34fe890a6c9cfd56f48af90564c0774b
Lucas Azevedo <102688981+Fuguety@users.noreply.github.com>
2023-11-14 11:31:55 +0100

    Add files via upload

---
+++ Dada Bank C#/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Data
+{
+    public class main
+    {
+
+        public static void Main()
+        {
+
+            var dataBanks = new LiteDB.LiteDatabase("bank.db");
+
+            var collection = dataBanks.GetCollection<Person>("people");
+
+            var people = collection.Query().ToList();
+
+            foreach (var item in people)
+            {
+                Console.WriteLine(item.Name);
+            }
+
+            var person = new Person()
+            {
+                Id = 1,
+                Name = "Fallen",
+                CPF = "123456789",
+                BirthDate = DateTime.Parse("01/01/2000")
+            };
+            
+            collection.Insert(person);
+
+
+        }
+    }
+}
